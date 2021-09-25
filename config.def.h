82e576ba78188b295fa6ddf065baba4b532f5630
liulei <liu6uu@163.com>
2021-09-25 11:23:48 +0800

    add: newterm patch

--- config.def.h
+++ config.def.h
@@ -208,6 +208,7 @@ static Shortcut shortcuts[] = {
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ MODKEY,               XK_c,           normalMode,     {.i =  0} },
+	{ MODKEY,               XK_Return,      newterm,        {.i =  0} },
 };
 
 /*
